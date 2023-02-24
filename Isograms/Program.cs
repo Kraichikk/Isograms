f88a3b7ba4120b759d67a3765a678ec7787f4615
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 14:50:15 +0200

    Add project files.

---
+++ Isograms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Isograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsIsogram("abc"));
+        }
+        public static bool IsIsogram(string s)
+        {
+            string str = s.ToLower();
+            int counter = 0;
+            char val = ' ';
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                val = str[i];
+
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if ((j != i) && (val == str[j]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
